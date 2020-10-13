16c2837d1ca071522d8b9200b8ae9d57c89213d3
jaithind <jaideepthind99@gmail.com>
2020-10-13 13:24:03 -0500

    Updates account.py
    Defined methods of cart classes. makePurchase is still to be done.

--- Packages/account.py
+++ Packages/account.py
@@ -1,11 +1,37 @@
+import items
+
 class User:
+
     # TODO: User class goes here
     pass
 
 
 class Cart:
-    # TODO: Cart class goes here
-    pass
+	def __init__(self, username):
+		self.__items_list = []
+		self.__username = username
+		self.__total_price = 0
+
+	def calculate_total(self):
+		for x in self.__items_list:
+			self.__total_prices+=x.getPrice()
+
+	def updateQuantity(self, item, new_quantity):
+		for x in self.__items_list:
+			if x == item:
+				return x.setQuantityInCart(new_quantity)
+			else:
+				return False
+
+	def getTotalPrice(self):
+		return self.__total_price
+
+	def getAllItems(self):
+		return self.__items
+
+	def makePurchase(self, shipping_address, credit_card_number):
+		pass
+    # TODO: makePurchase class here
 
 
 class Purchase:
